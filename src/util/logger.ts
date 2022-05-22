f3321f47c56f19f5d6c7e4510e2fd7c6fa017eda
Brendt Bly <brendt.bly@outlook.com>
2022-05-22 16:02:32 -0500

    Fixed gitignore.

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,11 +1,20 @@
 import winston from "winston";
+import { existsSync, mkdirSync } from 'fs'
+import { join } from 'path'
+
+const logDir = 'logs'
+
+if ( !existsSync( logDir ) ) {
+    // Create the directory if it does not exist
+    mkdirSync( logDir );
+}
 
 const options: winston.LoggerOptions = {
     transports: [
         new winston.transports.Console({
             level: process.env.NODE_ENV === "production" ? "error" : "debug"
         }),
-        new winston.transports.File({ filename: "debug.log", level: "debug" })
+        new winston.transports.File({ filename: join(logDir, '/log.log'), level: "debug" })
     ]
 };
 
