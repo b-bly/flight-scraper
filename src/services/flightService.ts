2ad2daa96d132cc2ebd829ebc07998af37cf2f5c
Brendt Bly <brendt.bly@outlook.com>
2022-06-02 08:35:46 -0500

    getFlights route

--- src/services/flightService.ts
+++ src/services/flightService.ts
@@ -5,4 +5,14 @@ export default class FlightService {
     const flight = new Flight(data)
     await flight.save()
   }
+
+  static async getFlights(page = 1) {
+    if (page < 1) {
+      throw new Error('pages must be more than one')
+    }
+    const skip = (page - 1) * 20
+    await Flight.find()
+      .skip(10)
+      .limit(20)
+  }
 }
